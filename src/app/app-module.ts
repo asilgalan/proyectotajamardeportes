99537eae54c3c6b2565354b483f87a9b4aa44948
mmtol <mmigueltolosana@gmail.com>
2026-01-13 18:37:31 +0100

    perfil

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,17 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { LoginComponent } from './auth/components/loginComponent/loginComponent';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { authInterceptor } from './auth/interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
-    App
+    App,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,LoginComponent
+
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ],
   bootstrap: [App]
 })
