7ccef6a0d47aedc889c466bdf8e0b2f810b9043f
mmtol <mmigueltolosana@gmail.com>
2026-01-18 18:41:32 +0100

    perfil mostrando los eventos y actividades

--- src/app/components/perfil.component/perfil.component.ts
+++ src/app/components/perfil.component/perfil.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import Perfil from '../../models/perfil';
 import ServicePerfil from '../../services/perfil.service';
 import Swal from 'sweetalert2';
+import {evento} from '../../models/evento';
 
 
 @Component
@@ -16,43 +17,61 @@ export class PerfilComponent implements OnInit
 {
   public perfil!:Perfil
 
-  public eventos:Array<number> = new Array<number>
-  public actividadesEvento!:Array<number>
+  public eventos:Array<evento> = new Array<evento>
+  idEventoSeleccionado: number | null = null;
 
-  //ejemplo
-  idEvento: number | null = null;
+  public nombreActividad!:string;
+  public quiereSerCapitan!:boolean;
+  public fechaInscripcion!: string;
 
-  constructor(private _service:ServicePerfil) 
-  {
-    //prueba eventos
-    this.eventos.push(1)
-    this.eventos.push(2)
-    this.eventos.push(3)
-
-    this.idEvento = 1;
-  }
+  constructor(private _servicePerfil:ServicePerfil) {}
 
   ngOnInit(): void 
   {
-    this._service.getPerfil().then(response =>
+    this._servicePerfil.getPerfil().then(response =>
     {
       this.perfil = response;
 
-      //hay que sacar los eventos por usuario
+      this._servicePerfil.getEventosYActividades().then(response =>
+      {
+        for (let i = 0; i < response.length; i++) 
+        {
+          const ev = new evento(
+            response[i].idEvento,
+            response[i].fechaEvento, 0);
+
+          this.eventos.push(ev);
+        }
+      })
     })
   }
 
-  seleccionarEvento(evento: any) 
+ seleccionarEvento(evento: number) 
   {
-    // Si ya estaba seleccionado, lo colapsamos
-    if (this.idEvento == evento) 
+    if (this.idEventoSeleccionado === evento) 
     {
-      this.idEvento = null;
-    } 
-    else 
-    {
-      this.idEvento = evento;
+      this.idEventoSeleccionado = null;
+      this.nombreActividad = '';
+      this.quiereSerCapitan = false;
+      this.fechaInscripcion = '';
+      return;
     }
-  }
 
+    this._servicePerfil.getEventosYActividades().then(response => 
+    {
+      for (let i = 0; i < response.length; ++i) 
+      {
+        if (response[i].idEvento == evento) 
+        {
+          this.idEventoSeleccionado = evento;
+
+          this.nombreActividad = response[i].nombreActividad;
+          this.quiereSerCapitan = response[i].quiereSerCapitan;
+          this.fechaInscripcion = response[i].fechaInscripcion;
+
+          console.log(this.idEventoSeleccionado);
+        }
+      }
+    });
+  }
 }
