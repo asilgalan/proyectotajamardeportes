8af27862101ee02797beb1077182bd43414beb0a
alooonsogm <alonsogarciamartin2@gmail.com>
2026-01-18 21:14:25 +0100

    arreglo routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import Perfil from './models/perfil';
 import { LoginComponent } from './auth/components/loginComponent/loginComponent';
 import { PerfilComponent } from './components/perfil.component/perfil.component';
 import { HomeComponent } from './components/home.component/home.component';
